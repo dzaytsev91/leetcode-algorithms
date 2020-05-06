df01232c4208aad7aa4ede01b4e4d916808072f4
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2020-05-06 21:40:53 +0300

    144 binary preorder traversal

---
+++ medium/144_binary_preorder_traversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        stack = [root]
+        result = []
+        while stack:
+            node = stack.pop()
+            if node:
+                result.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return result
