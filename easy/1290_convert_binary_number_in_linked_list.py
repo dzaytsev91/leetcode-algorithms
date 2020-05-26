bc29958c5add073ef00a13c7fd7686b2f1a475f9
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2020-05-26 19:54:18 +0300

    easy 1290 binary number in linked list

---
+++ easy/1290_convert_binary_number_in_linked_list.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        binary = ""
+        while head:
+            binary += str(head.val)
+            head = head.next
+        return int(binary, 2)
