134563c5c8db4113c4ac10c9e21a8c316e10d0ef
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2020-06-25 18:54:34 +0300

    medium design a stac with an increment operation

---
+++ medium/1381_desing_a_stack_with_increment_operation.py
@@ -0,0 +1,28 @@
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.maxSize = maxSize
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.maxSize:
+            self.stack.append(x)
+
+    def pop(self) -> int:
+        if self.stack:
+            return self.stack.pop()
+        else:
+            return -1
+
+    def increment(self, k: int, val: int) -> None:
+        for index in range(k):
+            try:
+                self.stack[index] += val
+            except IndexError:
+                return
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
