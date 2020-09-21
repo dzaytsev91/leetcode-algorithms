dddb55d57b9a9592502e1c8b254c0cc664c89ef1
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2020-09-21 20:50:09 +0300

    1302 medium deepest leaves sum BFS

---
+++ medium/1302_deepest_leaves_sum.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        stack = [root]
+        max_sum = 0
+        while stack:
+            values = []
+            local_sum = 0
+            while stack:
+                root = stack.pop()
+                if root.left:
+                    values.append(root.left)
+                if root.right:
+                    values.append(root.right)
+                local_sum += root.val
+            stack.extend(values)
+            max_sum = local_sum
+        return max_sum
