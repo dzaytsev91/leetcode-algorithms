f2b8248f593956ddc1536464e6030fab3aaab546
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-02-16 11:21:21 +0300

    fix easy 1337

--- easy/1337_the_k_weakest_rows_in_a_matrix.py
+++ easy/1337_the_k_weakest_rows_in_a_matrix.py
@@ -3,23 +3,10 @@ from typing import List
 
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        if not mat:
-            return []
-        response = []
-        max_col = len(mat[0]) - 1
+        tmp = []
+        for i,m in enumerate(mat):
+            cand = (sum(m), i)
+            tmp.append(cand)
 
-        while max_col >= 0:
-            max_row = len(mat) - 1
-            while max_row >= 0:
-                if mat[max_row][max_col] == 1:
-                    response.insert(0, max_row)
-                    mat[max_row] = [9] * len(mat[0])
-                max_row -= 1
-            max_col -= 1
-
-        rows = len(mat) - 1
-        while rows >= 0:
-            if mat[rows][0] == 0:
-                response.insert(0, rows)
-            rows -= 1
-        return response[:k]
+        tmp.sort()
+        return [i[1] for i in tmp]
