342e080de773cdad4589ddffba1938f622009ba8
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-02-17 20:06:55 +0300

    new var 11 medium

--- medium/11_container_with_most_water.py
+++ medium/11_container_with_most_water.py
@@ -1,15 +1,17 @@
+from typing import List
+
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        max_result = 0
-        a_pointer = 0
-        b_pointer = len(height)-1
-        while a_pointer < b_pointer:
-            if height[a_pointer] < height[b_pointer]:
-                max_result = max(max_result, height[a_pointer] * (b_pointer - a_pointer))
-                a_pointer += 1
+        max_area = 0
+        start_index = 0
+        end_index = len(height) - 1
+        while (start_index - end_index) != 0:
+            max_area = max(max_area, min(height[start_index], height[end_index]) * (end_index - start_index))
+            if height[start_index] < height[end_index]:
+                start_index += 1
             else:
-                max_result = max(max_result, height[b_pointer] * (b_pointer - a_pointer))
-                b_pointer -= 1
-        return max_result
+                end_index -= 1
 
+        return max_area
 
