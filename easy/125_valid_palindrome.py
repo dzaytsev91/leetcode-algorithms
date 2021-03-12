5ffe85ec9708152630ff1c456d235f978e2eb1f2
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-03-12 20:49:14 +0300

    easy 125 valid palindrome

--- easy/125_valid_palindrome.py
+++ easy/125_valid_palindrome.py
@@ -1,13 +1,15 @@
 class Solution:
-
     def isPalindrome(self, s: str) -> bool:
-        clear = [x.lower() for x in s if x.isalnum()]
+        letters = []
+        for letter in s:
+            if letter.isalpha() or letter.isdigit():
+                letters.append(letter.lower())
 
-        if clear == "":
-            return True
-
-        letters = clear[::]
-        for letter in clear:
-            if letter != letters.pop():
+        left, right = 0, len(letters) - 1
+        while left < right:
+            if letters[left] != letters[right]:
                 return False
+            else:
+                left += 1
+                right -= 1
         return True
