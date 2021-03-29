d10b987e3ee96c072aae3b092c0a6f8a4018c558
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-03-29 20:36:19 +0300

    48 rotate image fix

--- medium/48_rotate_image.py
+++ medium/48_rotate_image.py
@@ -3,13 +3,19 @@ from typing import List
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        if not matrix:
-            return
-        length = len(matrix[0])
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        for row in range(len(matrix)):
+            for col in range(row):
+                matrix[row][col], matrix[col][row] = matrix[col][row], \
+                                                     matrix[row][col]
 
-        for row in range(length):
-            for col in range(row, length):
-                matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
+        for row in matrix:
+            left = 0
+            right = len(row) - 1
+            while left < right:
+                row[left], row[right] = row[right], row[left]
+                left += 1
+                right -= 1
 
-        for row in range(length):
-            matrix[row].reverse()
