fa4d817a4552a78988c323278d1f4d43ba18b9af
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-03-30 20:41:57 +0300

    easy 374

---
+++ easy/374_guess_number_higher_or_lower.py
@@ -0,0 +1,21 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+def guess(num: int) -> int:
+    pass
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left = 0
+        right = n
+        while left <= right:
+            mid = left + (right - left) // 2
+            number = guess(mid)
+            if number == 0:
+                return mid
+            elif number == 1:
+                left = mid + 1
+            else:
+                right = mid - 1
+
