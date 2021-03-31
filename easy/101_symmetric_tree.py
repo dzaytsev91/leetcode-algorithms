b13c3c4cfaff39821a307b6e008670335e0447f4
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-03-31 12:22:13 +0300

    easy 101 symmetric tree

--- easy/101_symmetric_tree.py
+++ easy/101_symmetric_tree.py
@@ -1,18 +1,27 @@
 # Definition for a binary tree node.
 class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        if root is None:
-            return True
-        return self.check(root.left, root.right)
+        stack = [(root.left, root.right)]
 
-    def check(self, left, right):
-        if right and left:
-            return left.val == right.val and self.check(left.right, right.left) and self.check(left.left, right.right)
-        return left == right
+        while stack:
+            node1, node2 = stack.pop()
+
+            if node1 is None and node2 is None:
+                continue
+            if node1 is None or node2 is None:
+                return False
+            if node1.val != node2.val:
+                return False
+
+            stack.append([node1.left, node2.right])
+            stack.append([node1.right, node2.left])
+
+        return True
+
