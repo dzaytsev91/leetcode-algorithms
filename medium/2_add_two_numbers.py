ce1d77351d232fe7b3b98e44a69d952e5a997f31
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-04-01 18:14:25 +0300

    medium 2 right version

--- medium/2_add_two_numbers.py
+++ medium/2_add_two_numbers.py
@@ -2,32 +2,43 @@
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
-        self.next = next
+        self.next = next\
 
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        result = ListNode(0)
-        temp = result
-        carry = 0
+        if not l1 and not l2:
+            return [0]
+
+        result = ListNode()
+        tmp = result
+        reminder = 0
 
         while l1 or l2:
-            l1_val = l1.val if l1 else 0
-            l2_val = l2.val if l2 else 0
-            current_sum = l1_val + l2_val + carry
-            carry = current_sum // 10
-            last_digit = current_sum % 10
-
-            temp.next = ListNode(val=last_digit)
-
-            temp = temp.next
+            total_sum = 0
             if l1:
+                total_sum += l1.val
                 l1 = l1.next
             if l2:
+                total_sum += l2.val
                 l2 = l2.next
-        if carry > 0:
-            node = ListNode(val=1)
-            temp.next = node
+
+            total_sum += reminder
+
+            if total_sum > 9:
+                reminder = 1
+                new_node = ListNode(val=total_sum - 10)
+            else:
+                new_node = ListNode(val=total_sum)
+                reminder = 0
+
+            tmp.next = new_node
+            tmp = new_node
+
+        if reminder > 0:
+            new_node = ListNode(val=reminder)
+            tmp.next = new_node
 
         return result.next
+
 
