6010248597f033081107f56ddedd7c18ea6fe34c
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-04-17 16:46:48 +0300

    simpler version of 94 medium

--- medium/94_binary_tree_inorder_traversal.py
+++ medium/94_binary_tree_inorder_traversal.py
@@ -1,28 +1,23 @@
 # Definition for a binary tree node.
-from typing import List
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
-        result = []
-        stack = []
-        if root == None:
-            return result
-
-        while stack or root != None:
-            while root != None:
-                stack.append(root)
-                root = root.left
-
-            root = stack.pop()
-            result.append(root.val)
-            root = root.right
-        return result
+    def inorderTraversal(self, root):
+        res = []
+        stack = [(root, False)]
+        while stack:
+            node, visited = stack.pop()
+            if node is None:
+                continue
+            if visited:
+                res.append(node.val)
+            else:
+                # left -> root -> right
+                stack.append([node.right, False])
+                stack.append([node, True])
+                stack.append([node.left, False])
+        return res
 
