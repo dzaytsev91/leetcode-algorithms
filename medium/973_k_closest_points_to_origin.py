5be1f860ec838078e3ca3881efa7fd7f2121cef4
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-05-01 15:45:06 +0300

    973 medium

---
+++ medium/973_k_closest_points_to_origin.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        orig = [0, 0]
+        result = []
+        data = {}
+        for point in points:
+            distance = point[0] ** 2 + point[1] ** 2
+            if distance in data:
+                data[distance].append(point)
+            else:
+                data[distance] = [point]
+
+        for key, val in sorted(data.items()):
+            for p in val:
+                result.append(p)
+                if len(result) == k:
+                    return result
