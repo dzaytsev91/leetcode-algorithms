7f91c0105f09183fb5cfce1cc4fe4caac3fd7de6
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-05-02 20:26:28 +0300

    easy 70 fix

--- easy/70_clumbing_stairs.py
+++ easy/70_clumbing_stairs.py
@@ -1,16 +1,16 @@
 class Solution:
-    def __init__(self):
-        self.cache = {1: 1, 2: 2}
-
-    def count_steps(self, n):
-        if self.cache.get(n):
-            return self.cache.get(n)
-        if n == 0 or n == 1:
-            return 1
-
-        result = self.count_steps(n - 1) + self.count_steps(n - 2)
-        self.cache[n] = result
-        return result
 
     def climbStairs(self, n: int) -> int:
-        return self.count_steps(n)
+        cache = {}
+
+        def rec(n):
+            if cache.get(n):
+                return cache.get(n)
+            if n == 0 or n == 1:
+                return 1
+            result = rec(n - 1) + rec(n - 2)
+            cache[n] = result
+            return result
+
+        return rec(n)
+
