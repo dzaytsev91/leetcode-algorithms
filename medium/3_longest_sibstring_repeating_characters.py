66a8fdfe3d5327d52db61b0a644806b9781bec3a
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-05-04 22:05:37 +0300

    medium 3 my version

--- medium/3_longest_sibstring_repeating_characters.py
+++ medium/3_longest_sibstring_repeating_characters.py
@@ -1,16 +1,23 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen = {}
+        if not s:
+            return 0
         left = 0
-        result = 0
-        for r in range(len(s)):
-            if s[r] not in seen:
-                result = max(result, r - left + 1)
-            else:
-                if seen[s[r]] < left:
-                    result = max(result, r - left + 1)
-                else:
-                    left = seen[s[r]] + 1
-            seen[s[r]] = r
+        right = 0
+        max_sub = float('-inf')
+        current_sub = {}
+        while right < len(s):
+            if s[right] in current_sub:
+                while s[left] != s[right]:
+                    current_sub.pop(s[left])
+                    left += 1
+                current_sub.pop(s[left])
+                left += 1
 
-        return result
+            current_sub[s[right]] = 1
+            right += 1
+
+            max_sub = max(max_sub, len(current_sub))
+
+        return max_sub
+
