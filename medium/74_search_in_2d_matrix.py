46b8d14543e1ba8bf359d67ec9e1feaad1314361
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-05-27 18:54:51 +0300

    medium 74

--- medium/74_search_in_2d_matrix.py
+++ medium/74_search_in_2d_matrix.py
@@ -2,11 +2,20 @@ from typing import List
 
 
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for index in range(len(matrix)):
-            if matrix[index][-1] >= target:
-                for indx in range(len(matrix[index])):
-                    if matrix[index][indx] == target:
-                        return True
-                return False
+    def hasNumber(self, nums, target):
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = left + (right-left) // 2
+            if nums[mid] == target:
+                return True
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
         return False
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+            if row[-1] >= target:
+                return self.hasNumber(row, target)
