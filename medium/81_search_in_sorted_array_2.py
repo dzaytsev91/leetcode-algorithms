a90c5d563becf3c42cefed1c9ba74a1cabdfc0b1
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-05-28 19:12:19 +0300

    medium 81

--- medium/81_search_in_sorted_array_2.py
+++ medium/81_search_in_sorted_array_2.py
@@ -6,22 +6,21 @@ class Solution:
         left = 0
         right = len(nums)-1
         while left <= right:
+            while left < right and nums[left] == nums[left+1]:
+                left += 1
+            while right > left and nums[right] == nums[right-1]:
+                right -= 1
             mid = left + (right-left)//2
-
             if nums[mid] == target:
                 return True
-
-            while left < mid and nums[left] == nums[mid]:
-                left += 1
-
-            if nums[left] <= nums[mid]:
+            if nums[mid] >= nums[left]:
                 if nums[left] <= target <= nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
             else:
                 if nums[mid] <= target <= nums[right]:
-                    left += 1
+                    left = mid + 1
                 else:
                     right = mid - 1
         return False
