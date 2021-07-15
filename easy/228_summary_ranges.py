fc45e0834151e8554060bfdd1080b5ed774cbdf2
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2021-07-15 23:33:24 +0300

    clean version easy 228

--- easy/228_summary_ranges.py
+++ easy/228_summary_ranges.py
@@ -2,28 +2,31 @@ from typing import List
 
 
 class Solution:
-    def formatter(self, f, l):
-        if f == l:
-            return str(f)
-        return "{}->{}".format(f, l)
+    def format_seq(self, start, end):
+        if start == end:
+            return str(end)
+        return f"{start}->{end}"
 
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        if not nums:
-            return []
-        if len(nums) < 2:
-            return [str(nums[0])]
-        nums.sort()
+        start_group = None
+        end_group = None
         result = []
-        start_interval = nums[0]
-        end_interval = nums[0]
-        for index in range(1, len(nums)):
-            if nums[index] == end_interval + 1:
-                end_interval = nums[index]
-            else:
-                result.append(self.formatter(start_interval, end_interval))
-                start_interval = nums[index]
-                end_interval = nums[index]
 
-        result.append(self.formatter(start_interval, end_interval))
+        for num in nums:
+            if start_group is None:
+                start_group = num
+                end_group = num
+                continue
+
+            if num - end_group == 1:
+                end_group = num
+            else:
+                result.append(self.format_seq(start_group, end_group))
+                start_group = num
+                end_group = num
+
+        if start_group is not None and end_group is not None:
+            result.append(self.format_seq(start_group, end_group))
 
         return result
+
