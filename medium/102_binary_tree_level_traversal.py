5463da37b53c3f77c759a0a4b5e0ea26a20b3557
Logovskii Dmitrii <d-zaytsev@yandex-team.ru>
2021-12-04 13:13:37 +0300

    102 medium

--- medium/102_binary_tree_level_traversal.py
+++ medium/102_binary_tree_level_traversal.py
@@ -1,5 +1,8 @@
+# idea: using BFS we should add nodes to stack level by level
+# than agg lvl values and add to result
+
 # Definition for a binary tree node.
-from typing import List
+from typing import List, Optional
 
 
 class TreeNode:
@@ -8,21 +11,26 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
         result = []
-        stack = [root]
+        stack = [(root,)]
         while stack:
-            level_result = []
-            nodes = []
-            while stack:
-                node = stack.pop(0)
-                if not node:
-                    continue
-                nodes.append(node.left)
-                nodes.append(node.right)
-                level_result.append(node.val)
-            stack = nodes
-            if level_result:
-                result.append(level_result)
+            level = stack.pop()
+            current_lvl_values = []
+            next_lvl = []
+            for node in level:
+                current_lvl_values.append(node.val)
+                if node.left:
+                    next_lvl.append(node.left)
+                if node.right:
+                    next_lvl.append(node.right)
+
+            if current_lvl_values:
+                result.append(current_lvl_values)
+            if next_lvl:
+                stack.append(next_lvl)
         return result
