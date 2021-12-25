a65e8f9234f21411a444deb9219a8809fc02eca9
Logovskii Dmitrii <d-zaytsev@yandex-team.ru>
2021-12-25 16:08:23 +0300

    236 medium

---
+++ medium/236_lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,27 @@
+# idea: make a recursive dfs call for left and right branch
+# and check where we find any of p or q
+# if we find p and q in both, that means this node is ancestor
+# if only left, that means left is ancestor if right means right
+
+
+# Definition for a binary tree node.
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode',
+                             q: 'TreeNode') -> 'TreeNode':
+        if root in (None, p, q):
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left or right
+
