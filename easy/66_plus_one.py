08fbf737eb94b33446fb107863e6847cd6cf5fc1
k-zaytsev <37955247+k-zaytsev@users.noreply.github.com>
2022-10-12 14:32:14 +0300

    Start this
    Так лучше сделать

--- easy/66_plus_one.py
+++ easy/66_plus_one.py
@@ -3,16 +3,14 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        index = len(digits) - 1
-
-        for num in range(len(digits)):
-            if digits[index] == 9:
-                digits[index] = 0
-                index -= 1
-                continue
-            else:
-                digits[index] += 1
-                return digits
-
-        digits.insert(0, 1)
+        i = len(digits)-1
+        digits[i] += 1
+        while i > 0:
+            if digits[i] == 10:
+                digits[i] = 0
+                digits[i-1] += 1
+            i-=1
+        if digits[0] == 10:
+            digits[0] = 0
+            digits.insert(0, 1)
         return digits
