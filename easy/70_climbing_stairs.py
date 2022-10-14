5986a60a315885428b66304639a1e389ac3eebf5
Logovskii Dmitrii <still65@yandex.ru>
2022-10-14 12:54:57 +0300

    Create 70_climbing_stairs.py
    70 easy

---
+++ easy/70_climbing_stairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+        for index in range(n -1):
+            tmp = one
+            one = one + two
+            two = tmp
+        return one
